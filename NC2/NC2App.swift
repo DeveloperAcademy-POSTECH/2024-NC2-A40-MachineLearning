2762b1882485d32f1c49ba0df2d22731fd231c0f
JeEon Park <ggungtoo@icloud.com>
2024-06-15 03:59:36 +0900

    Initial Commit

---
+++ NC2/NC2App.swift
@@ -0,0 +1,17 @@
+//
+//  NC2App.swift
+//  NC2
+//
+//  Created by DevJonny on 2024/6/15.
+//
+
+import SwiftUI
+
+@main
+struct NC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
